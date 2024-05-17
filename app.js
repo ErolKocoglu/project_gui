c63f48439448f3aece97f5a4e399f03f7c05dbbc
ErolKocoglu <erolkocoglu44@gmail.com>
2024-05-17 15:27:56 +0300

    Add onclick operation

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
 
 // variable to store our intervalID
 let nIntervId;
+let nabiz =document.querySelector("#heart_rate");
+console.log(nabiz);
+nabiz.addEventListener("click", increase);
+function increase(){
+  let heart = document.getElementById("heart_rate");
+  let num=parseInt(heart.innerHTML);
+  heart.innerHTML=num +10;
+
+}
 
 function changeColor() {
   // check if an interval has already been set up
   if (!nIntervId) {
-    nIntervId = setInterval(execute, 1000);
+    nIntervId = setInterval(execute, 5000);
   }
 }
+
 
 function execute() {
   let heart_rate = document.getElementById("heart_rate");
